c809090055c6f2f1d3d8c6a6a1de4cb0e3702e68
Code SDK <codesdktutorials@gmail.com>
2015-07-29 05:27:47 -0400

    CRUD on users

--- README.md
+++ README.md
@@ -13,18 +13,18 @@
 
 - mongod --dbpath ~/mongodb/data/db &
 
-- Change default route in config/routes.js from 
+- Change default route in config/routes.js from
 
 view: 'homepage'
 
-to 
+to
 
 view: 'static/index'
 
 - layout.ejs is the wrapper of content that's not going to change page-to-page
 - whereas the views are going to be the dynamic content which is going to change
 - So, static/index is the dynamic view which is inserted in layout.ejs where it says  <%- body %> in layout.ejs
-- sails wraps the layout.ejs file around the index.ejs file 
+- sails wraps the layout.ejs file around the index.ejs file
 
 
 - The linker designation when we created the project
@@ -32,10 +32,10 @@ view: 'static/index'
 
 As a convenience, all the javascript you put under js and all the styles you put under styles will be compiled
 and minified via this layout.ejs file when sails lifts
-This is a way we can use twitter bootstrap and drop our twitter bootstrap javascript and css files and 
+This is a way we can use twitter bootstrap and drop our twitter bootstrap javascript and css files and
 they are available on every page via layout.ejs
 
-### To generate user model and controller 
+### To generate user model and controller
 sails generate api user
 
 Enable module.exports.csrf = true; in config/csrf.js
@@ -43,12 +43,10 @@ Enable module.exports.csrf = true; in config/csrf.js
 Turn on the following blueprints in config/blueprints.js : prefix, actions, shortcuts, rest
 ### Improve error handling
 
-Change UserController.js to have better error handling 
+Change UserController.js to have better error handling
 
 
 ### Client-side validation with policies
 
 Policies are middleware functions which run before controllers
 Any of these policies can be applied to a given controller or one or more of its actions within the controller
-
-
--- cooporg/api/controllers/UserController.js
+++ cooporg/api/controllers/UserController.js
@@ -1,9 +1,9 @@
 /**
- * UserController
- *
- * @description :: Server-side logic for managing users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+* UserController
+*
+* @description :: Server-side logic for managing users
+* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+*/
 
 module.exports = {
 	'new': function(req, res) {
@@ -27,7 +27,99 @@ module.exports = {
 			}
 			// After successfully creating the user
 			// redirect to the show action
-			res.json(user);
+			// res.json(user);
+			res.redirect('/user/show/'+user.id);
+		});
+	},
+	// render the profile view (ex /views/show.ejs)
+	'show': function(req, res, next) {
+		User.findOne(req.param('id'), function foundUser(err, user) {
+			if (err) return next(err);
+			if (!user) return next();
+			res.view({
+				user: user
+			});
+		});
+		/*
+		var id = req.param('id');
+		User.findOne()
+		.where({id: id})
+		.then(function(err, user){
+			if (err) return next(err);
+			if (!user) {
+				return next();
+			}
+			else {
+				res.view({
+					user: user
+				});
+			}
+		})
+		.catch(function(err){
+			res.badRequest('Something went wrong!');
+		});
+		*/
+	},
+	'index': function(req, res, next) {
+		// Get an array of all the users in the User collection
+		User.find(function foundUsers(err, users) {
+			if (err) return next(err);
+			//pass the array down to the /views/index.ejs page
+			res.view({
+				users:users
+			});
+		});
+	},
+	// render the edit view (ex /views/edit.ejs)
+	'edit': function(req, res, next) {
+		// Find the user from the id passed in via params
+		User.findOne(req.param('id'), function foundUser(err, user) {
+			if (err) return next(err);
+			if (!user) return next();
+			res.view({
+				user: user
+			});
+		});
+	},
+	'update': function (req,res) {
+
+	var params = _.extend(req.query || {}, req.params || {}, req.body || {});
+	var id = params.id;
+
+	if (!id) return res.send("No id specified.",500);
+
+	User.update(id, params, function userUpdated(err, updatedUser) {
+		if (err) {
+			res.redirect('/user/edit');
+		}
+		if(!updatedUser) {
+			res.redirect('/user/edit');
+		}
+		res.redirect('/user/show/'+id);
+	});
+},
+	// process the info from edit view
+	/*
+	'update': function(req, res, next) {
+		console.log(req.params());
+
+		console.log('update was called');
+		User.update(req.param('id'), req.params.all(), function userUpdated(err) {
+			if (err) {
+				// return res.redirect('/user/edit/' + req.param('id'));
+			}
+			// res.redirect('/user/show/' + req.param('id'));
+		});
+	}
+	*/
+	destroy: function(req, res, next) {
+		User.findOne(req.param('id'), function foundUser(err, user) {
+			if (err) return next(err);
+			if (!user) return next('User doesn\'t exist');
+			User.destroy(req.param('id'), function userDestroyed(err) {
+				if (err) return next(err);
+			});
+		res.redirect('/user');
 		});
 	}
 };

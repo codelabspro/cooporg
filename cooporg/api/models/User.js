40bbda816ba8cc50747732c70aed59cb05b8d943
Code SDK <codesdktutorials@gmail.com>
2015-07-30 02:02:31 -0400

    Fixed encryptedPassword issue

--- cooporg/api/models/User.js
+++ cooporg/api/models/User.js
@@ -30,19 +30,18 @@ module.exports = {
       delete obj.encryptedPassword;
       delete obj._csrf;
       return obj;
-    },
-    beforeCreate: function (values, next) {
-
-     // This checks to make sure the password and password confirmation match before creating record
-     if (!values.password || values.password != values.confirmation) {
-       return next({err: ["Password doesn't match password confirmation."]});
-     }
-
-     require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encryptedPassword) {
-       if (err) return next(err);
-       values.encryptedPassword = encryptedPassword;
-       next();
-     });
+    }
+  },
+  beforeCreate: function (values, next) {
+   // This checks to make sure the password and password confirmation match before creating record
+   if (!values.password || values.password != values.confirmation) {
+     return next({err: ["Password doesn't match password confirmation."]});
    }
-  }
+
+   require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encryptedPassword) {
+     if (err) return next(err);
+     values.encryptedPassword = encryptedPassword;
+     next();
+   });
+ }
 };
